0204d0340a89e7f9f7b7fe92fa01f327149525f6
lxb <424151385@qq.com>
2017-05-25 14:28:31 +0800

    test

--- db/migrate/20170522061142_add_portfolio_reference_to_technology.rb
+++ db/migrate/20170522061142_add_portfolio_reference_to_technology.rb
@@ -1,5 +1,5 @@
 class AddPortfolioReferenceToTechnology < ActiveRecord::Migration[5.1]
   def change
-    add_reference :technologies, :portfolio, type: :integer, foreign_key: true, index: true
+    add_reference :technologies, :portfolio, foreign_key: true, index: true
   end
 end
